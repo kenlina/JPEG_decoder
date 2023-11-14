9759b248de685cb8e421890c5316fabe23ce2985
kenlina <a0961368835@gmail.com>
2023-11-14 15:23:41 +0800

    將read_JPEG完成，改在argv讀入參數

--- main.cpp
+++ main.cpp
@@ -3,29 +3,46 @@
 using namespace std;
 #include<vector>
 
+void read_JPEG(char *argv, vector<unsigned char> &buffer);
+
 int main(int argc, char *argv[]){
-    ifstream img("/home/ken/Desktop/112_1/ITCT/Image/gig-sn01.jpg",ios::binary);
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <image path>" << endl;
+        return 1;
+    }
+    //*************將讀到的資料存在buffer中***********
+    vector<unsigned char> buffer;
+    read_JPEG(argv[1],buffer);
+
+    return 0;
+}
+
+void read_JPEG(char *argv, vector<unsigned char> &buffer){
+    //**********Open image************
+    ifstream img(argv,ios::binary); 
+
+    //**********Record image length************
     img.seekg(0,ios::end);
     size_t imgSize = img.tellg();
     img.seekg(0,ios::beg);
 
+
     if(!img){
         cerr << "Can't open image." << endl;
-        return 1;
+        exit (1);
     }
 
-    vector<unsigned char> buffer(imgSize);
+    buffer.resize(imgSize);
     img.read(
         reinterpret_cast<char*> ( buffer.data() ),
         buffer.size()
     );
-    for(unsigned char byte : buffer){
-        cout << hex <<  static_cast<int>(byte) << " ";
-    }
-    cout << endl;
+    // for(unsigned char byte : buffer){
+    //     cout << hex <<  static_cast<int>(byte) << " ";
+    // }
+    // cout << endl;
 
     cout << dec << "The image size is " << imgSize << endl;
 
     img.close();
-    return 0;
 }
